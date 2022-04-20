b6e814981237282615b0c22ff12d143fe61cb822
Ståle Dahl <stalehd@gmail.com>
2022-04-20 19:26:17 +0200

    Script to run program

---
+++ server.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Launch the report-and-possibly-download every 30 seconds
+while 1; do
+    ./fota-sample
+    # If there is a new image it will be downloaded to image.new. Rename to fota-sample
+    # to use the new version.
+    if [ -f image.new ]; then
+        echo "**** A new version is available!"
+        mv fota-sample fota-sample.old
+        mv image.new fota-sample
+    fi
+    sleep 30
+done
